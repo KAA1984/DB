aa5f807c5bad1382937396cd965efd3e1fbf6ff8
KAA1984 <andrii.kalistratov@gmail.com>
2022-04-10 22:04:10 +0300

    update create table

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -1,7 +1,10 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.ParseException;
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 
 public class CreateTable {
@@ -10,7 +13,7 @@ public class CreateTable {
     private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/test";
     private static final String DB_USER = "admin";
     private static final String DB_PASSWORD = "root";
-    //private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd hh24:mi:ss");
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     //private static final DateFormat dateFormat = DateFormat.getDateInstance();
         ///dd-MMM-yyyy HH:mm:ss
     public static void main(String[] argv) {
@@ -44,10 +47,10 @@ public class CreateTable {
         String insert = "INSERT INTO DBUSERS"
                 + "(USER_ID, USERNAME, CREATED_BY, CREATED_DATE) "
                 + "VALUES"
-                + "('2','user2','admin', "
+                + "('5','user5','admin', "
                 + " str_to_date('"
                 + getCurrentTimeStamp()
-                + "','%d.%m.%Y %h:%i:%s'))";
+                + "','yyyy-MM-dd HH:mm:ss'))";
 
         //String select = "SELECT * from dbusers";
 
@@ -122,9 +125,13 @@ public class CreateTable {
     }
 
     private static String getCurrentTimeStamp(){
-        //DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        /*DateFormat dateFormat = DateFormat.getDateTimeInstance();
         DateFormat dateFormat = DateFormat.getDateInstance();
         Date today = new Date();
+        return dateFormat.format(today.getTime());
+
+         */
+        java.util.Date today = new java.util.Date();
         return dateFormat.format(today.getTime());
     }
 }
